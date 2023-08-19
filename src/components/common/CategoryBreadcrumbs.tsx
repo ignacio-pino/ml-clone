11afa92b8b9dbc6c02e9584104cbf52f7bf037da
Ignacio <pino.v.pablo@gmail.com>
2023-08-19 01:52:15 -0300

    [6] added category breadcrumbs component

---
+++ src/components/common/CategoryBreadcrumbs.tsx
@@ -0,0 +1,43 @@
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
+import { IPathFromRoot } from "../../Interfaces";
+import { createSearchParams, useNavigate } from "react-router-dom";
+
+export default function CategoryBreadcrumbs({
+  pathFromRoot,
+  categoryId,
+}: {
+  pathFromRoot: IPathFromRoot[];
+  categoryId: string;
+}) {
+  const navigate = useNavigate();
+
+  const handleClick = (event: React.MouseEvent) => {
+    event?.preventDefault();
+    const params = { category: event.currentTarget.id };
+    navigate({
+      pathname: "/products",
+      search: `${createSearchParams(params)}`,
+    });
+  };
+
+  return (
+    <Breadcrumb
+      spacing="8px"
+      separator={<ChevronRightIcon color="gray.500" />}
+      fontSize={"smaller"}
+      maxW={"70%"}
+    >
+      {pathFromRoot?.map(({ id, name }) => {
+        return id === categoryId ? null : (
+          <BreadcrumbItem>
+            <BreadcrumbLink id={id} onClick={handleClick}>
+              {" "}
+              {name}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        );
+      })}
+    </Breadcrumb>
+  );
+}
